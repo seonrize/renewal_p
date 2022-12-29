35e7cf4e0e8f44d84073c47f6824dc3fcd6005c2
kimserim <112158792+imserimkim@users.noreply.github.com>
2022-12-29 10:24:44 +0900

    12/29
    branch 올리기

--- page/gallery.js
+++ page/gallery.js
@@ -1,43 +1,43 @@
 // pop-up창 활성화 
-const elPopup = document.querySelector('.pop-up'),
-    elBtn = document.querySelector('.title01 button'),
-    elExit = document.querySelector('.pop-up button'); 
+// const elPopup = document.querySelector('.pop-up'),
+//     elBtn = document.querySelector('.title01 button'),
+//     elExit = document.querySelector('.pop-up button'); 
 
 
-elBtn.onclick=function(){
-        elPopup.classList.add('active');
-}
+// elBtn.onclick=function(){
+//         elPopup.classList.add('active');
+// }
 
 
-elPopup.onclick = function(e){
+// elPopup.onclick = function(e){
 
-    if(e.target.classList.contains('pop-up'))
-    {elPopup.classList.remove('active');}
+//     if(e.target.classList.contains('pop-up'))
+//     {elPopup.classList.remove('active');}
 
-    console.log(e);
-        elExit.onclick = function(){
-            elPopup.classList.remove('active');
-        }
-}
+//     console.log(e);
+//         elExit.onclick = function(){
+//             elPopup.classList.remove('active');
+//         }
+// }
 
-//썸네일 활성화 
+// //썸네일 활성화 
 
-// let data=[
-//     {url:'../img/2f.jpg'},
-//     {url:'../img/1f.jpg'},
-//     {url:'../img/b1f.jpg'},
-//     {url:'../img/b2f.jpg'},
-// ]
+// // let data=[
+// //     {url:'../img/2f.jpg'},
+// //     {url:'../img/1f.jpg'},
+// //     {url:'../img/b1f.jpg'},
+// //     {url:'../img/b2f.jpg'},
+// // ]
 
-// const elThumb = document.querySelector('span'),
-//         elImg = document.querySelector('.thumb');
+// // const elThumb = document.querySelector('span'),
+// //         elImg = document.querySelector('.thumb');
 
-//         console.log(elThumb);
+// //         console.log(elThumb);
         
-//         let tag = '';
+// //         let tag = '';
 
-//             elThumb.addEventListener('mouseover', (event) => {
-//                 elImg.style.display="block";
-//             });
+// //             elThumb.addEventListener('mouseover', (event) => {
+// //                 elImg.style.display="block";
+// //             });
 
 
