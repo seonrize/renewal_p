ee8b90752b76d709d5b10897bcdb174772d9a984
kimserim <112158792+imserimkim@users.noreply.github.com>
2023-01-02 18:22:11 +0900

    01/02-2

--- page/gallery.js
+++ page/gallery.js
@@ -1,4 +1,4 @@
-// pop-up창 활성화 
+//  section-scroll pop-up창 활성화 
     // const elPopup = document.querySelector('.pop-up'),
     //     elBtn = document.querySelector('.title01 button'),
     //     elExit = document.querySelector('.pop-up button'); 
@@ -20,28 +20,58 @@
     //         }
     // }
 
-// //썸네일 활성화 
-
-    // // let data=[
-    // //     {url:'../img/2f.jpg'},
-    // //     {url:'../img/1f.jpg'},
-    // //     {url:'../img/b1f.jpg'},
-    // //     {url:'../img/b2f.jpg'},
-    // // ]
-
-    // // const elThumb = document.querySelector('span'),
-    // //         elImg = document.querySelector('.thumb');
-
-    // //         console.log(elThumb);
-            
-    // //         let tag = '';
-
-    // //             elThumb.addEventListener('mouseover', (event) => {
-    // //                 elImg.style.display="block";
-    // //             });
-
-
-
 
 
 // load 
+
+
+const elMain = document.querySelector('main'),
+    elAside = document.querySelector('aside'),
+    elSec = document.querySelectorAll('section');
+let move = 0;
+
+
+elSec.forEach(function (elSection, key) {
+    elAside.innerHTML += '<button></button>';
+    elSection.addEventListener('mousewheel', function () {
+        animation(key);
+    })
+})
+
+const elBtns = document.querySelectorAll('aside button'); 
+    let idx = 0;
+elBtns.forEach(function(btn,key){
+    btn.addEventListener('click', function(){
+
+        animation(key)
+    })
+})
+
+function update(key){
+    elBtns[idx].classList.remove('active');  // 저번 키를 지우고
+    elBtns[key].classList.add('active');   // 현재 클릭한 새로운 액티브를 넣어줘 
+        idx=key;
+}
+
+    update(0);  // 처음 실행되는 콘텐츠의 값 
+
+function animation(key){    
+    try {
+        if (event.wheelDelta < 0) {
+            move = elSec[key].nextElementSibling.offsetTop;
+            key ++;
+        } else if(event.wheelDelta > 0) {                         // wheel 했을때 (if, else if)
+            move = elSec[key].previousElementSibling.offsetTop;
+            key --;
+        }else{
+            move = elSec[key].offsetTop;     //click 했을 때 
+        }
+    } catch { }
+    update(key);
+
+
+
+    elMain.style = `transform:translateY(-${move}px)`;
+    console.log(move);
+    console.log(elAside);
+}
