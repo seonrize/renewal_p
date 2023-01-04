fa43b73f4802bda40d5b246fe99c76572f8f0773
Namgyu Kim <wkfkem@naver.com>
2023-01-04 13:52:39 +0900

    asdasdasdasd

--- page/letter.js
+++ page/letter.js
@@ -2,6 +2,7 @@ let taskInput = document.getElementById("task-input");
 let taskInput1 = document.getElementById("task-input1");
 let addButton = document.getElementById("add-button");
 let moveButton = document.getElementById("move-button");
+let mySwiper = document.getElementById("my-siper");
 let taskList = [];
 let filterList = [];
 
@@ -13,33 +14,22 @@ function enterkey() {
   }
 }
 function changeBtnName() {
-   const moveText = `<span>편지쓰러가기</span>
+  const moveText = `<span class="move-font">편지쓰러가기</span>
      <span class="masker">
-     <span>편지쓰러가기</span>
+     <span class="move-font">편지쓰러가기</span>
      </span>`;
-   const moveText1 = `<span>편지함가기</span>
+  const moveText1 = `<span class="move-font">편지함가기</span>
      <span class="masker">
-     <span>편지함가기</span>
+     <span class="move-font">편지함가기</span>
      </span>`;
-   let n = 30;
-   if(window.scrollY < n){
-     window.scrollTo(0,940);
-     moveButton.innerHTML = moveText;
-   }else{
-     window.scrollTo(0,0);
-     moveButton.innerHTML = moveText1;
-   }
-}
-
-window.addEventListener('scroll', function(){
-  console.log( window.scrollY)
-})
-
-function showSpinner() {
-  document.getElementsByClassName("layerPopup")[0].style.display = "block";
-}
-function hideSpinner() {
-  document.getElementsByClassName("layerPopup")[0].style.display = "none";
+  let n = 30;
+  if (window.scrollY < n) {
+    window.scrollTo(0, 940);
+    moveButton.innerHTML = moveText;
+  } else {
+    window.scrollTo(0, 0);
+    moveButton.innerHTML = moveText1;
+  }
 }
 
 function addTask() {
@@ -49,19 +39,29 @@ function addTask() {
     taskContent1: taskInput1.value,
   };
   if (document.getElementById("task-input", "add-button").value == "") {
-    alert("이름을 입력하세요");
+    Swal.fire({
+      icon: "warning",
+      text: "이름을 입력하세요.",
+    });
     return;
   }
   if (document.getElementById("task-input1", "add-button").value == "") {
-    alert("메세지를 입력하세요");
+    Swal.fire({
+      icon: "warning",
+      text: "메세지를 입력하세요.",
+    });
     return;
   }
   taskList.push(task);
   document.getElementById("task-input", "add-button").value = "";
   document.getElementById("task-input1", "add-button").value = "";
-  showSpinner();
-  setTimeout(() => hideSpinner(), 200);
-  setTimeout(() => render()(alert("편지가 전송되었습니다.")), 300);
+  Swal.fire({
+    icon: "success",
+    title: "편지가 전송되었습니다.",
+    showConfirmButton: false,
+    timer: 900,
+  });
+  render();
 }
 
 function render() {
@@ -69,55 +69,93 @@ function render() {
   list = taskList;
   let resultHTML = "";
   for (let i = 0; i < list.length; i++) {
-    if (list[i].isComplete) {
-      resultHTML += `<div class="task" id="${list[i].id}">
-      <div class="button-box">
-      <button class="trash" onclick="deleteTask('${list[i].id}')" tabindex="-1"><span class="material-symbols-outlined">
-      delete
-      </span></button>
-      </div>
-      <div class="div-flex"> 
-      <span>${list[i].taskContent}</span>
-      </div>
-      <div class="div-flex1"> 
-      <textarea readonly="readonly">${list[i].taskContent1}</textarea>
-      </div>
-       </div>`;
-    } else {
-      resultHTML += `<div class="task" id="${list[i].id}" >
+    resultHTML += `<div class="swiper-slide" id="${list[i].id}" >
+    <div class="img-con3" id="${list[i].id}">
+    <img class="retter01" src="./img/letter01.png">
+    </div>
+    <div class="img-con4" id="${list[i].id}">
+    <img class="retter02" src="./img/letter02.png">
+    </div>
+      <div class="task">
       <div class="button-box">  
       <button class="trash" onclick="deleteTask('${list[i].id}')" tabindex="-1"><span class="material-symbols-outlined">
       delete
       </span></button>
       </div>
-      <div class="div-flex">  
-      <span>${list[i].taskContent}</span> 
+      <div class="div-flex">
+      <span class="toFrom">${list[i].taskContent}</span> 
       </div>
-      <div class="div-flex1">  
-      <textarea readonly="readonly">${list[i].taskContent1}</textarea> 
+      <div class="div-flex1">
+      <textarea class="toFrom" readonly="readonly">${list[i].taskContent1}</textarea> 
+      </div>
       </div>
   </div>`;
-    }
   }
+  swiper();
   document.getElementById("task-board").innerHTML = resultHTML;
 }
 
 function deleteTask(id) {
-  let confirm_val = confirm("정말 삭제하시겠습니까?");
-  if (confirm_val == true) {
-    for (let i = 0; i < taskList.length; i++) {
-      if (taskList[i].id == id) {
-        filterList.splice(i, 1);
-        taskList.splice(i, 1);
-        break;
-      } else {
+  Swal.fire({
+    title: "삭제하시겠습니까?",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      for (let i = 0; i < taskList.length; i++) {
+        if (taskList[i].id == id) {
+          filterList.splice(i, 1);
+          taskList.splice(i, 1);
+          break;
+        } else {
+        }
       }
+      render();
+      Swal.fire("완료", "삭제되었습니다.");
     }
-    render();
-  } else if (confirm_val == false) {
-  }
+  });
 }
 
 function randomIDGenerate() {
   return "_" + Math.random().toString(36).substring(2, 9);
 }
+
+function swiper() {
+  mySwiper = new Swiper(".mySwiper", {
+    slidesPerView: "3", // 한 슬라이드에 보여줄 갯수
+    loopAdditionalSlides: 1,
+    // 슬라이드 반복 시 마지막 슬라이드에서 다음 슬라이드가 보여지지 않는 현상 수정
+    pagination: {
+      // 호출(pager) 여부
+      el: ".swiper-pagination", //버튼을 담을 태그 설정
+      clickable: true, // 버튼 클릭 여부
+    },
+    navigation: {
+      // 버튼
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      360: {
+        slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+        slidesPerGroup: 1,
+      },
+    },
+  });
+}
+swiper();
